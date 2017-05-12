98fc481afd17fd1b0cecfb3a884898010d4a70c8
sunwhale <sunwhale@126.com>
2017-05-12 18:03:41 +0800

    1.0.0

---
+++ README .md
@@ -0,0 +1,7 @@
+# catia
+
+1.编辑arguments.txt文件，根据需要修改响应的参数。
+
+2.运行Catia.exe，生成CATScript，html，qrcode文件夹和create_titleblock.bat，rename.bat批处理文件。  
+
+3.name_space.xlsx为所需excel文件。  
--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-copy GB_Titleblock.CATScript .\dist
-copy arguments.txt .\dist
-copy name_space.xlsx .\dist
 python setup.py install
 echo off python setup.py py2exe
 python setup.py py2exe --dll-excludes=MSVCP90.DLL
+copy GB_Titleblock.CATScript .\dist
+copy arguments.txt .\dist
+copy name_space.xlsx .\dist
